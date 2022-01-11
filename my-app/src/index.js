df3346fea544de3256d562f7307d79c1c61471a3
Admin <iryna2001ka@gmail.com>
2022-01-11 23:11:35 +0200

    Lesson-47

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,20 +5,13 @@ import './index.css';
 import App from './App';
 import {Provider} from "react-redux";
 
-let rerenderEntireTree = () => {
-    ReactDOM.render(
-        <React.StrictMode>
-            <Provider store={store}>
-                <App/>
-            </Provider>
-        </React.StrictMode>,
-        document.getElementById('root')
-    )
-}
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App/>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+)
 
-rerenderEntireTree();
-
-store.subscribe(() => {
-    rerenderEntireTree();
-});
 
