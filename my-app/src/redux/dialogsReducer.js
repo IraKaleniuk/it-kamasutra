3cbd6e993665599ad0bdd9fc9062f6a46319943b
Admin <iryna2001ka@gmail.com>
2022-01-12 00:06:57 +0200

    Lesson-48

--- my-app/src/redux/dialogsReducer.js
+++ my-app/src/redux/dialogsReducer.js
@@ -15,25 +15,28 @@ let initialState = {
         {id: 4, message: 'Yo'}
     ],
     newMessageText: ''
-}
+};
 
 const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case SEND_MESSAGE: {
             let newMessage = {
-                id: 5,
-                message: state.newMessageText,
+                id: 5, message: state.newMessageText,
             };
-            let stateCopy = {...state};
-            stateCopy.messages = [...state.messages];
-            stateCopy.messages.push(newMessage);
-            stateCopy.newMessageText = '';
-            return stateCopy;
+            return ({
+                    ...state,
+                    messages: [...state.messages, newMessage],
+                    newMessageText: ''
+                }
+            );
         }
         case UPDATE_NEW_MESSAGE_TEXT: {
-            let stateCopy = {...state};
-            stateCopy.newMessageText = action.newText;
-            return stateCopy;
+            return (
+                {
+                    ...state,
+                    newMessageText: action.newText
+                }
+            );
         }
         default:
             return state;
