97f734396afa9a4b3e54641d8c8dac8a5d3af1ff
Admin <iryna2001ka@gmail.com>
2022-01-23 15:40:43 +0200

    lesson-55

--- my-app/src/components/users/Users.jsx
+++ my-app/src/components/users/Users.jsx
@@ -5,14 +5,70 @@ import userPhoto from '../../assets/images/user.png';
 
 class Users extends React.Component {
     componentDidMount() {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users", {
+            params: {
+                page: this.props.currentPage,
+                count: this.props.pageSize
+            }
+        }).then(response => {
+            this.props.setUsers(response.data.items)
+            this.props.setTotalUsersCount(response.data.totalCount)
+        })
+    }
+
+    onPageChange = (p) => {
+        this.props.setCurrentPage(p);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users", {
+            params: {
+                page: p,
+                count: this.props.pageSize
+            }
+        }).then(response => {
             this.props.setUsers(response.data.items)
         })
     }
 
+    generatePageRange(currentPage, lastPage) {
+        const delta = 1;
+
+        const range = [];
+        for (let i = Math.max(2, (currentPage - delta)); i <= Math.min((lastPage - 1), (currentPage + delta)); i += 1) {
+            range.push(i);
+        }
+
+        if ((currentPage - delta) > 2) {
+            range.unshift('...');
+        }
+        if ((currentPage + delta) < (lastPage - 1)) {
+            range.push('...');
+        }
+
+        range.unshift(1);
+        if (lastPage !== 1) range.push(lastPage);
+
+        return range;
+    }
+
     render() {
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+
         return (
             <div>
+                <div>
+                    {
+                        this.generatePageRange(this.props.currentPage, pagesCount)
+                            .map(p => {
+                                return <span className={this.props.currentPage === p ? styles.selectedPage : ""}
+                                             onClick={(e) => this.onPageChange(p)}>-{p}-</span>
+                            })
+                    }
+                </div>
                 {
                     this.props.users.map(u => <div key={u.id}>
                     <span>
