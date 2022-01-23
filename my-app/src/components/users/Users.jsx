77c0e3e8fd5c145314c7c88e5405632b4f8d1497
Admin <iryna2001ka@gmail.com>
2022-01-23 17:16:12 +0200

    lesson-56

--- my-app/src/components/users/Users.jsx
+++ my-app/src/components/users/Users.jsx
@@ -1,76 +1,22 @@
 import React from "react";
 import styles from './Users.module.css';
-import * as axios from 'axios';
 import userPhoto from '../../assets/images/user.png';
 
-class Users extends React.Component {
-    componentDidMount() {
-        axios.get("https://social-network.samuraijs.com/api/1.0/users", {
-            params: {
-                page: this.props.currentPage,
-                count: this.props.pageSize
+let Users = (props) => {
+
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+    return (
+        <div>
+            {
+                props.generatePageRange(props.currentPage, pagesCount)
+                    .map(p => {
+                        return <span
+                            className={props.currentPage === p ? styles.selectedPage : styles.page}
+                            onClick={(e) => props.onPageChange(p)}>{p}</span>
+                    })
             }
-        }).then(response => {
-            this.props.setUsers(response.data.items)
-            this.props.setTotalUsersCount(response.data.totalCount)
-        })
-    }
-
-    onPageChange = (p) => {
-        this.props.setCurrentPage(p);
-        axios.get("https://social-network.samuraijs.com/api/1.0/users", {
-            params: {
-                page: p,
-                count: this.props.pageSize
-            }
-        }).then(response => {
-            this.props.setUsers(response.data.items)
-        })
-    }
-
-    generatePageRange(currentPage, lastPage) {
-        const delta = 1;
-
-        const range = [];
-        for (let i = Math.max(2, (currentPage - delta)); i <= Math.min((lastPage - 1), (currentPage + delta)); i += 1) {
-            range.push(i);
-        }
-
-        if ((currentPage - delta) > 2) {
-            range.unshift('...');
-        }
-        if ((currentPage + delta) < (lastPage - 1)) {
-            range.push('...');
-        }
-
-        range.unshift(1);
-        if (lastPage !== 1) range.push(lastPage);
-
-        return range;
-    }
-
-    render() {
-
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-
-        let pages = [];
-        for (let i = 1; i <= pagesCount; i++) {
-            pages.push(i);
-        }
-
-        return (
-            <div>
-                <div>
-                    {
-                        this.generatePageRange(this.props.currentPage, pagesCount)
-                            .map(p => {
-                                return <span className={this.props.currentPage === p ? styles.selectedPage : styles.page}
-                                             onClick={(e) => this.onPageChange(p)}>{p}</span>
-                            })
-                    }
-                </div>
-                {
-                    this.props.users.map(u => <div key={u.id}>
+            {
+                props.users.map(u => <div key={u.id}>
                     <span>
                         <div>
                             <img src={u.photos.small != null ? u.photos.small : userPhoto} alt={'photo'}
@@ -78,14 +24,14 @@ class Users extends React.Component {
                         </div>
                         <div>
                             {u.followed ? <button onClick={() => {
-                                    this.props.unfollow(u.id)
+                                    props.unfollow(u.id)
                                 }}>Unfollow</button>
                                 : <button onClick={() => {
-                                    this.props.follow(u.id)
+                                    props.follow(u.id)
                                 }}>Follow</button>}
                         </div>
                     </span>
-                        <span>
+                    <span>
                         <span>
                             <div>{u.name}</div>
                             <div>{u.status}</div>
@@ -95,11 +41,11 @@ class Users extends React.Component {
                             <div>{"u.location.city"}</div>
                         </span>
                     </span>
-                    </div>)
-                }
-            </div>
-        )
-    }
+                </div>)
+            }
+        </div>
+    )
 }
+
 
 export default Users;
--- my-app/src/components/users/UsersContainer.jsx
+++ my-app/src/components/users/UsersContainer.jsx
@@ -1,7 +1,65 @@
 import React from "react";
 import {connect} from "react-redux";
-import Users from "./Users";
 import {followAC, setCurrentPageAC, setUsersAC, unfollowAC, setTotalUsersCountAC} from "../../redux/usersReducer";
+import Users from "./Users";
+import * as axios from "axios";
+
+class UsersContainer extends React.Component {
+    componentDidMount() {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users", {
+            params: {
+                page: this.props.currentPage,
+                count: this.props.pageSize
+            }
+        }).then(response => {
+            this.props.setUsers(response.data.items)
+            this.props.setTotalUsersCount(response.data.totalCount)
+        })
+    }
+
+    onPageChange = (p) => {
+        this.props.setCurrentPage(p);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users", {
+            params: {
+                page: p,
+                count: this.props.pageSize
+            }
+        }).then(response => {
+            this.props.setUsers(response.data.items)
+        })
+    }
+    generatePageRange = (currentPage, lastPage) => {
+        const delta = 1;
+
+        const range = [];
+        for (let i = Math.max(2, (currentPage - delta)); i <= Math.min((lastPage - 1), (currentPage + delta)); i += 1) {
+            range.push(i);
+        }
+
+        if ((currentPage - delta) > 2) {
+            range.unshift('...');
+        }
+        if ((currentPage + delta) < (lastPage - 1)) {
+            range.push('...');
+        }
+
+        range.unshift(1);
+        if (lastPage !== 1) range.push(lastPage);
+
+        return range;
+    }
+
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      pageSize={this.props.pageSize}
+                      generatePageRange={this.generatePageRange}
+                      currentPage={this.props.currentPage}
+                      onPageChange={this.onPageChange}
+                      users={this.props.users}
+                      unfollow={this.props.unfollow}
+                      follow={this.props.follow}/>
+    }
+}
 
 const mapStateToProps = (state) => {
     return ({
@@ -32,6 +90,5 @@ const mapDispatchToProps = (dispatch) => {
     })
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users)
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer)
 
-export default UsersContainer
